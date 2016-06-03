a62d7c94b23bb692781b9c6771880c50b4a8cf2c
Martin Musale <martinmshale@gmail.com>
2016-06-03 21:44:40 +0300

    setup stripe to get the token

--- src/app/angular2-stripe.component.ts
+++ src/app/angular2-stripe.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+declare var Stripe:any;
 
 @Component({
   moduleId: module.id,
@@ -6,6 +7,44 @@ import { Component } from '@angular/core';
   templateUrl: 'angular2-stripe.component.html',
   styleUrls: ['angular2-stripe.component.css']
 })
-export class Angular2StripeAppComponent {
-  title = 'angular2-stripe works!';
+export class Angular2StripeAppComponent implements OnInit{
+  title = 'Stripe Demo';
+  private cardToken:any;
+
+  ngOnInit(){
+    this.setUpCard();
+  }
+
+  setUpCard() {
+    //here we setup the stripe publish key.
+    //notice that this is a test key for my account so replace with production key(live)
+    Stripe.setPublishableKey('pk_test_1JJKhZ3DycRrYqdE5GWzlbDd');
+  }
+
+  getCardData(number, month, year, cvc) {
+    //I get the card data typed in here and pass it to the getCardToken method
+    this.getCardToken(number, month, year, cvc);
+  }
+
+  getCardToken(number, month, year, cvc) {
+    //set up the card data as an object
+    var dataObj = {"number": number, "exp_month": month, "exp_year": year, "cvc": cvc};
+
+    // Request a token from Stripe:
+    Stripe.card.createToken(dataObj,
+      (status, response) => { //I'm using an arrow function instead of stripeResponseHandler(a function also) cos it's kickass!
+        // basically you can do anything here with the reponse that has your token
+        // you can hit your backend api and initialize a charge etc
+        if (status === 200) {
+          console.log("the card response: ", response);
+          this.cardToken = response.id;
+          console.log("the caard token: ", this.cardToken);
+        }
+        else {
+          console.log("error in getting card data: ", response.error)
+        }
+      }
+    );
+
+  }
 }
